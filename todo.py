2f899cf784e38006a22d1b3131bedcc34d198fc7
jzarzycki <jzarzycki97@gmail.com>
2019-10-19 13:21:44 +0200

    update todo

--- todo.py
+++ todo.py
@@ -1,9 +1,10 @@
-# clean up code
+# clean up code, refactor how animations are handled
 # add dictionary with animation settings in data directory
 # add exception handling
 # add listening for a device
 # add controller mobile app or website
 # add better animations
+# add midi keyboard support
 
 # ideas
     # argparse?
